5f0a84c43de155562aded179ec3883f1c8275fbd
Kmiokande <brunnodesouzabezerra@gmail.com>
2021-06-20 23:53:50 -0300

    Finalizando primeiro dia com rotas GET e POST

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,12 @@ import express from 'express';
 
 const app = express();
 
+app.get('/test', (request, response) => {
+  return response.send("Olá NLW");
+});
+
+app.post('/test-post', (request, response) => {
+  return response.send("Olá NLW método POST");
+});
+
 app.listen(3000, () => console.log("🚀 Server is running"));
