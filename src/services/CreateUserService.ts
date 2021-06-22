4ab4435812704ebae724f2d8f4503a0d9b0ebe65
Kmiokande <brunnodesouzabezerra@gmail.com>
2021-06-22 13:16:04 -0300

    Criando services

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,39 @@
+import { UsersRepositories } from '../repositories/UsersRepositories';
+
+import { User } from '../entities/User';
+
+interface IUserRequest {
+  name: string;
+  email: string;
+  admin?: boolean;
+}
+
+class CreateUserService {
+  async execute({ name, email, admin }: IUserRequest): Promise<User> {
+    const usersRepository = new UsersRepositories();
+
+    if (!email) {
+      throw new Error('Email incorrect');
+    }
+
+    const usersAlreadyExists = await usersRepository.findOne({
+      email,
+    });
+
+    if (usersAlreadyExists) {
+      throw new Error('User already exists');
+    }
+
+    const user = usersRepository.create({
+      name,
+      email,
+      admin,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export { CreateUserService };
