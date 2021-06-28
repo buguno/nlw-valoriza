475fda5b628b232145d9062de254c5bcb6688f89
Kmiokande <brunnodesouzabezerra@gmail.com>
2021-06-28 17:55:46 -0300

    Adiconando ssl no typeormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,7 @@
 module.exports = {
   "type": "postgres",
   "url": process.env.DATABASE_URL,
+  "ssl": true,
   "migrations": [
     `${(process.env.MODE === "prod") ? "dist/database/migrations/*.js" : "src/database/migrations/*.ts"}`
   ],
