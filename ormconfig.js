8551be4e995407351a6c30b55e88cbb9e753e7a3
Kmiokande <brunnodesouzabezerra@gmail.com>
2021-06-28 18:03:53 -0300

    Adicionando extras

--- ormconfig.js
+++ ormconfig.js
@@ -2,6 +2,11 @@ module.exports = {
   "type": "postgres",
   "url": process.env.DATABASE_URL,
   "ssl": true,
+  "extra": {
+    "ssl": {
+      "rejectUnauthorized": false,
+    },
+  },
   "migrations": [
     `${(process.env.MODE === "prod") ? "dist/database/migrations/*.js" : "src/database/migrations/*.ts"}`
   ],
